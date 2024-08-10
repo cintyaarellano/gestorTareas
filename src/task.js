f1bec717ad5b588b60a4650c7e02bf874b8fa613
Cintya Arellano <carobbz@hotmail.com>
2024-08-10 01:13:40 -0600

    Estilos CSS, función agregar tareas, función traer tareas y función para visualizar tareas, UI.js

---
+++ src/task.js
@@ -0,0 +1,32 @@
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+// Función para llevar las tareas
+export const getTask = () => tasks;
+
+// Función para agregar una tarea
+export const addTask = (task) => {
+    const newTask = {
+        id: Date.now(),
+        "text": task,
+        completed: false,
+    };
+    tasks.push(newTask)
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+// Función para eliminar una tarea
+export const deleteTask = (id) => {
+    tasks = tasks.filter((taks) => task.id !== parseInt(id));
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+// Finción para actualizar una tarea
+export const toggleTask= (id) => {
+    tasks = tasks.map((task) => {
+        if(task.id === parseInt(id)) {
+            task.completed = !task.completed;
+        }
+        return task;
+    });
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
@@ -26,7 +27,9 @@ module.exports = {
     },
     devtool: 'source-map',
     devServer: {
-        contentBase: path.resolve(__dirname, 'dist'),
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
         compress: true,
         port: 9000,
     },
